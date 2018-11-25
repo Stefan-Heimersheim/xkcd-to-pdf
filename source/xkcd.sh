27abfa596e4955280ed207d44f1b6924d27f0589
Stefan Heimersheim <stefan.heimersheim@rwth-aachen.de>
2018-11-25 01:36:36 +0100

    Escapes dangerous characters now

--- source/xkcd.sh
+++ source/xkcd.sh
@@ -24,10 +24,17 @@ else
     wget https://xkcd.com/$1/ -O tmp/comic.html -q
     wget $(grep "Image URL" tmp/comic.html) -O tmp/image.png -q
     grep img\ src tmp/comic.html | grep title= | cut -d = -f 3 | cut -d \" -f 2 | recode html > tmp/hovertext.txt
-#   Works not in 1909 but in 1132:
-#   grep Title\ text tmp/comic.html | cut -d : -f 2 | cut -d \} -f 1 | sed 's/^ //' | recode html > tmp/hovertext.txt
     grep  \<title\> tmp/comic.html | cut -d : -f 2 | cut -d \< -f 1 | sed 's/^ //' > tmp/title.txt
     echo "https://xkcd.com/$1/" > tmp/url.txt
+    # Bad characters:  #, %, $, _, ^, &, {, }
+    sed -i 's/#/\\#/g' tmp/*.txt
+    sed -i 's/%/\\%/g' tmp/*.txt
+    sed -i 's/\$/\\$/g' tmp/*.txt
+    sed -i 's/_/\\_/g' tmp/*.txt
+    sed -i 's/\^/\\^/g' tmp/*.txt
+    sed -i 's/&/\\&/g' tmp/*.txt
+    sed -i 's/{/\\{/g' tmp/*.txt
+    sed -i 's/}/\\}/g' tmp/*.txt
     pdflatex -output-directory=tmp/ template.tex
     mv tmp/template.pdf bin/$1.pdf
     xdg-open bin/$1.pdf
