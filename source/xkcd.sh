1f571418cd57f68eb2529371981ccfc118630366
lennart <lennart@fsmpi.rwth-aachen.de>
2018-12-05 00:00:51 +0100

    Forked the script to be self-contained.

--- source/xkcd.sh
+++ source/xkcd.sh
@@ -1,41 +1,79 @@
 #!/bin/bash
-
-#    xkcd-gen, a script to generate pdfs from xkcd comics
-#    Copyright (C) 2018 Stefan Heimersheim
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-if [ $# -eq 0 ]
-  then
-    echo "Please give the xkcd ID as an argument"
+# depends on: bash, wget, curl, grep, python3, sed, pdflatex
+number="$1"
+# use current date if no number is given.
+if [ -z "$number" ]; then
+    siteurl="https://xkcd.com/"
+    number="$(date +'%d-%m-%Y')"
 else
-    rm tmp/* -f
-    wget https://xkcd.com/$1/ -O tmp/comic.html -q
-    wget $(grep "Image URL" tmp/comic.html) -O tmp/image.png -q
-    grep img\ src tmp/comic.html | grep title= | cut -d = -f 3 | cut -d \" -f 2 | recode html > tmp/hovertext.txt
-    grep  \<title\> tmp/comic.html | cut -d : -f 2 | cut -d \< -f 1 | sed 's/^ //' > tmp/title.txt
-    echo "https://xkcd.com/$1/" > tmp/url.txt
-    # Bad characters:  #, %, $, _, ^, &, {, }
-    sed -i 's/#/\\#/g' tmp/*.txt
-    sed -i 's/%/\\%/g' tmp/*.txt
-    sed -i 's/\$/\\$/g' tmp/*.txt
-    sed -i 's/_/\\_/g' tmp/*.txt
-    sed -i 's/\^/\\^/g' tmp/*.txt
-    sed -i 's/&/\\&/g' tmp/*.txt
-    sed -i 's/{/\\{/g' tmp/*.txt
-    sed -i 's/}/\\}/g' tmp/*.txt
-    pdflatex -output-directory=tmp/ template.tex
-    mv tmp/template.pdf bin/$1.pdf
-    xdg-open bin/$1.pdf
+    siteurl="https://xkcd.com/$number/"
 fi
+
+# function to decode url chars
+htmldecode() {
+    echo "$@" | python3 -c \
+        'import html, sys; [print(html.unescape(l), end="") for l in sys.stdin]'
+}
+
+# create tempdir
+directory="$(mktemp -d)"
+
+# download, decode etc.
+echo "$siteurl" > "$directory""/xkcd.url"
+wget --quiet "$siteurl" -O "$directory""/xkcd.html"
+htmldecode "$(grep "<img src.*title=.*alt=.*/>" "$directory""/xkcd.html" | \
+    grep -o 'title=".*" alt' | cut -c 8- | rev | cut -c 6- | \
+    rev)" > "$directory""/xkcd.txt"
+wget --quiet "$(grep hotlinking/embedding "$directory""/xkcd.html" | \
+    grep -o "https://.*png")" -O "$directory""/xkcd.png"
+htmldecode "$(grep "^<title" "$directory""/xkcd.html" | cut -c 14- | rev | \
+    cut -c 9- | rev)" > "$directory""/xkcd.title"
+echo "$number"":" > "$directory""/xkcd.num"
+
+# escape LaTeX-dangerous characters
+function escape_characters () {
+    sed -i 's/#/\\#/g' "$1"
+    sed -i 's/%/\\%/g' "$1"
+    sed -i 's/\$/\\$/g' "$1"
+    sed -i 's/_/\\_/g' "$1"
+    sed -i 's/\^/\\^/g' "$1"
+    sed -i 's/&/\\&/g' "$1"
+    sed -i 's/{/\\{/g' "$1"
+    sed -i 's/}/\\}/g' "$1"
+}
+escape_characters "$directory""/xkcd.title"
+escape_characters "$directory""/xkcd.txt"
+escape_characters "$directory""/xkcd.num"
+
+# create document.
+echo '
+\documentclass[12pt,a4paper]{article}
+\usepackage[margin=1cm]{geometry}
+\usepackage{graphicx}
+\usepackage[export]{adjustbox}
+\usepackage[utf8x]{inputenc}
+
+\begin{document}
+\pagenumbering{gobble}
+\begin{center}
+  \null
+  \vfill
+  {\Large\bfseries \input{xkcd.num}\input{xkcd.title}} \\
+  {\small\tt \input{xkcd.url}} \\[10pt]
+  \includegraphics[scale=0.7,max width=0.95\textwidth,max height=0.9\textheight]{xkcd.png} \\[10pt]
+  \parbox{0.7\textwidth}{
+    \itshape \input{xkcd.txt}
+  }
+  \vfill
+\end{center}
+\end{document}
+' > "$directory""/xkcd.tex"
+
+# cleanup and save to cwd.
+currentwdir="$(pwd)"
+cd "$directory"
+pdflatex xkcd.tex 1>/dev/null
+cd "$currentwdir"
+cp "$directory""/xkcd.pdf" "./xkcd-$number.pdf"
+rm -r "$directory"
+
